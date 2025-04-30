c74745f355a4c34ef06611bbb5636dcc1e8d18d3
Belceb45 <rubiodiego001@gmail.com>
2025-04-29 18:22:21 -0600

    Prueba MongoDB

---
+++ app/src/main/java/org/example/MongoDBConnection.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoDBConnection {
+
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+
+    public MongoDBConnection(String connectionString, String dbName) {
+        try {
+            ServerApi serverApi = ServerApi.builder()
+                    .version(ServerApiVersion.V1)
+                    .build();
+
+            MongoClientSettings settings = MongoClientSettings.builder()
+                    .applyConnectionString(new ConnectionString(connectionString))
+                    .serverApi(serverApi)
+                    .build();
+
+            // Crear el cliente y conectar a la base de datos
+            mongoClient = MongoClients.create(settings);
+            database = mongoClient.getDatabase(dbName);
+            System.out.println("Conexión exitosa a MongoDB");
+
+        } catch (MongoException e) {
+            e.printStackTrace();
+            System.err.println("Error al conectar con MongoDB: " + e.getMessage());
+        }
+    }
+
+    public MongoDatabase getDatabase() {
+        return database;
+    }
+
+    public void close() {
+        mongoClient.close();
+    }
+}
---
+++ app/src/main/java/org/example/ProductoService.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import java.util.Date;
+
+public class ProductoService {
+
+      private MongoCollection<Document> collection;
+
+    public ProductoService(MongoDatabase database) {
+        this.collection = database.getCollection("productos");
+    }
+
+    public void agregarProducto(String nombre, String descripcion, double precio, String categoria, int cantidadStock) {
+        Document producto = new Document("nombre", nombre)
+                .append("descripcion", descripcion)
+                .append("precio", precio)
+                .append("categoria", categoria)
+                .append("cantidad_stock", cantidadStock)
+                .append("fecha_creacion", new Date());
+
+        collection.insertOne(producto);
+        System.out.println("Producto agregado al catalogo: " + nombre);
+    }
+
+    public void encontrarProducto(String nombre) {
+        Document producto = collection.find(new Document("nombre", nombre)).first();
+        if (producto != null) {
+            System.out.println("Producto encontrado: " + producto.toJson());
+        } else {
+            System.out.println("Producto no encontrado.");
+        }
+    }
+}
